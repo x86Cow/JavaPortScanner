2c4f08449badfb987212692334cbd23b11b09987
Alex Lorei <DrCow@protonmail.com>
2023-03-07 17:07:59 +0000

    changed test IP

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         for(int p = 0; p < 1000; p++) {
-            ConnectionScanner.scanner("162.159.135.42", p, 20);
+            ConnectionScanner.scanner("142.250.217.110", p, 20);
         }
     }
 }
